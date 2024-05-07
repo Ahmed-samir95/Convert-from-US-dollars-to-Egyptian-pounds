b6c64aa335b1d3328b4ec2c746f3825946e14756
ahmed-samir <ahmedsamirr126@gmail.com>
2024-05-07 17:35:27 +0200

    modified js file

--- main .js
+++ main .js
@@ -3,31 +3,16 @@ let inp = document.querySelector("input")
 
 let result = document.querySelector(".result")
 
-let usaDollar = 40
+let usaDollar = 47.7700;
 
 inp.oninput = function() {
   let val = this.value
-  result.firstChild.innerText  = `{${val}} USA Dollar`;
-  result.lastChild.innerText  = `{${val * usaDollar}} Egyptian Pound`;
+  result.firstChild.innerText  = `{${val}} USD Dollar`;
+  result.lastChild.innerText  = `{${val * usaDollar}} USD Dollar`;
 
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
