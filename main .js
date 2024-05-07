d77e4def18a6bee74f7c2a479a86b0f14b9ff203
ahmed-samir <ahmedsamirr126@gmail.com>
2024-05-07 18:09:08 +0200

    keep modifing js file

--- main .js
+++ main .js
@@ -7,8 +7,8 @@ let usaDollar = 47.7700;
 
 inp.oninput = function() {
   let val = this.value
-  result.firstChild.innerText  = `{${val}} USD Dollar`;
-  result.lastChild.innerText  = `{${val * usaDollar}} USD Dollar`;
+  result.firstChild.innerText  = `{${val}} USA Dollar`;
+  result.lastChild.innerText  = `{${val * usaDollar}} Egyptian Pound`;
 
 
 
